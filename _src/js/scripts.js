b08b90794153b80ffbd1576b60b12d4913f8837e
Nic Chan <channicoleelise@gmail.com>
2017-02-15 05:46:18 +0000

    add footer waypoint, correct typos

--- _src/js/scripts.js
+++ _src/js/scripts.js
@@ -4,22 +4,29 @@ $(document).ready(function() {
   
   var waypoints = $('.showcase__section').waypoint({
     handler: function handler(direction) {
-      console.log('hit')
       var currentElement;
       if (direction === 'up'){
         currentElement = this.previous().element
       } else {
         currentElement = this.element
       }
-      console.log(currentElement);
       var currentImage = $(currentElement).find('.showcase__image-wrapper');
       currentImage.addClass('showcase__image-wrapper--active')
       $('.showcase__image-wrapper').not(currentImage).removeClass('showcase__image-wrapper--active')
     },
     offset: '50%',
   });
+
+   var footerInView = new Waypoint.Inview({
+    element: $('.footer'),
+    enter: function(direction) {
+      $('.showcase__image-wrapper').addClass('showcase__image-wrapper--stuck');
+    },
+    exited: function(direction) {
+      $('.showcase__image-wrapper').removeClass('showcase__image-wrapper--stuck');
+    },
+  })
   
-  console.log(waypoints)
 });
 
 lightbox.option({
